815b1a2254c18e7fab3e9f3548f94d2f2a827829
seoyeon <tjsus247@naver.com>
2022-09-02 20:59:10 +0900

    add errormessage

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
     const [newAccount,setNewAccount] = useState(true)
+    const [error,setError] = useState('')
     const onChange = (event) => {
         const { target:{name,value} } = event
         if(name==='email'){
@@ -31,7 +32,7 @@ const Auth = () => {
             }
             console.log(data)
         }catch(error){
-            console.log(error)
+            setError(error.message)
         }
        
     }
@@ -43,6 +44,7 @@ const Auth = () => {
                 <input name='password' type='password' placeholder='Passoword' required
                 value={password} onChange={onChange} />
                 <input type='submit' value={newAccount ? "Create Account" : "Log In"}  />
+                {error}
             </form>
             <div>
                 <button>Continue with Google</button>
