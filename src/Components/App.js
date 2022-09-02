52c5b93106e40effa5e04e8b45da6ca7aefacea3
seoyeon <tjsus247@naver.com>
2022-09-02 17:34:41 +0900

    add

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React,{useState} from 'react';
+import Router from './Router'
+
+function App() {
+  return (
+    <div >
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import {BrowserRouter, Routes, Route} from  "react-router-dom";
+import Auth from '../routes/Auth'
+import Home from '../routes/Home'
+
+const Router = () =>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    return(
+        <BrowserRouter>
+        <Routes>
+            {isLoggedIn ?(
+                <Route path="/" element={<Home/>} />
+            ) : ( 
+            <Route path="/" element={<Auth/>} /> 
+                )
+            }
+            
+           
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
